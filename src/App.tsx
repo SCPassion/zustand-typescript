b4c9dcc9919f57c58ba0a0e8b9bd2b5436343fc2
bernardcryptohk <bernard.cryptohk@gmail.com>
2025-05-11 21:50:14 +0200

    Refactor store setup to include Zustand devtools and add post management functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useUserStore } from "./store";
+import { useState } from "react";
+import { useUserStore, usePostStore } from "./store";
 
 // Updating the username & email in the store from a child compoent, input fields. Will change the state in the store
 // and the state in the parent component will also be updated automatically
@@ -25,6 +26,11 @@ function UpdateUserForm() {
 }
 function App() {
   const { username, email, setUsername, setEmail } = useUserStore(); // THis create a single source of store, which will be used in the entire app
+  const { posts, setPosts, addPost, removePost } = usePostStore(); // This will create a single source of store for posts
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [postIdCounter, setPostIdCounter] = useState(1); // This is a local state, which is not shared with the store
 
   return (
     <>
@@ -32,6 +38,44 @@ function App() {
       <h2>Username: {username}</h2>
       <h2>Email: {email}</h2>
       <UpdateUserForm />
+
+      <div>
+        <b>Create new post</b>
+      </div>
+      <input
+        type="text"
+        placeholder="Post title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Post title"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+
+      <button
+        onClick={() => {
+          addPost({ id: postIdCounter.toString(), title, content });
+          setPostIdCounter((prev) => prev + 1); // Increment the postIdCounter to create a new id for the next post
+          setTitle(""); // Clear the input field
+          setContent(""); // Clear the input field
+        }}
+      >
+        Add Post
+      </button>
+
+      <div>
+        <h1>Posts</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            <button onClick={() => removePost(post.id)}>Delete Post</button>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 export type UserStore = {
   username: string;
@@ -7,26 +8,63 @@ export type UserStore = {
   setEmail: (email: string) => void;
 };
 
-// store the single source of truth for the app
 // create a store to manage the user state, itself is a hook
-export const useUserStore = create<UserStore>((set) => ({
-  // set function is used to update the state in the store immutably
-  // initial state
-  username: "",
-  email: "",
+export const useUserStore = create(
+  devtools<UserStore>(
+    (set) => ({
+      // set function is used to update the state in the store immutably
+      // initial state
+      username: "",
+      email: "",
 
-  // function to update the state
-  setUsername: (username: string) =>
-    set(() => ({
-      // return the new state
-      username,
-      // We only need to update the username without spreading the rest of the state.
-      // Why? Zustand will merge the new state with the old state automatically.
-    })),
+      // function to update the state
+      setUsername: (username: string) =>
+        set(() => ({
+          // return the new state
+          username,
+          // We only need to update the username without spreading the rest of the state.
+          // Why? Zustand will merge the new state with the old state automatically.
+        })),
 
-  setEmail: (email: string) =>
-    set(() => ({
-      email,
-    })),
-  //
-}));
+      setEmail: (email: string) =>
+        set(() => ({
+          email,
+        })),
+      //
+    }),
+    { name: "user", store: "user" }
+  )
+);
+
+export type Post = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+export type PostsStore = {
+  posts: Post[];
+  setPosts: (posts: Post[]) => void;
+  addPost: (post: Post) => void;
+  removePost: (id: string) => void;
+};
+
+export const usePostStore = create(
+  devtools<PostsStore>(
+    (set) => ({
+      posts: [], // initial state
+      // setPosts function to update the posts state
+      setPosts: (posts: Post[]) => set(() => ({ posts: posts })), // no reference to the previous state, so we can just return the new state
+
+      addPost: (post: Post) =>
+        // addPost function to add a new post to the posts state
+        set((state) => ({ posts: [...state.posts, post] })), // immutably update the state by spreading the previous state and adding the new post
+
+      removePost: (id: string) =>
+        set((state) => ({
+          posts: state.posts.filter((post) => post.id !== id),
+        })),
+    }),
+    { name: "posts", store: "posts" }
+  )
+);
